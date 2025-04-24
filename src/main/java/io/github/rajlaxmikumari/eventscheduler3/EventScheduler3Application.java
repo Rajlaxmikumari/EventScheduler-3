7065c777b6d6ba1e2b65e16667bb54938a8c8c14
Rajlaxmikumari <rajlaxmikumari792@gmail.com>
2025-04-24 14:13:48 +0530

    Initial commit

---
+++ src/main/java/io/github/rajlaxmikumari/eventscheduler3/EventScheduler3Application.java
@@ -0,0 +1,13 @@
+package io.github.rajlaxmikumari.eventscheduler3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventScheduler3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventScheduler3Application.class, args);
+	}
+
+}
